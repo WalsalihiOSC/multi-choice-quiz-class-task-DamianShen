b5d342d15d60f0f35b8017445e1a5d6eefba4c0b
Damian Shen <65927110+DamianShen@users.noreply.github.com>
2022-04-05 11:03:04 +1200

    Created class and main routine

---
+++ multi_choice_quiz.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class MultiChoice:
+    def __init__(self, parent):
+        
+
+if __name__ == "__main__":
+    root = Tk()
+    buttons = MultiChoice(root)
+    root.title("Multi Choice Quiz")
+    root.mainloop()
