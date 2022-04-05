b103fc7a4098f78e45df2fdfe0ac092cbc19b93e
Damian Shen <65927110+DamianShen@users.noreply.github.com>
2022-04-05 12:07:27 +1200

    Created function to display answer

--- multi_choice_quiz.py
+++ multi_choice_quiz.py
@@ -3,7 +3,7 @@ from tkinter import *
 class MultiChoice:
     def __init__(self, parent):
         self.answer = StringVar()
-        self.answer.set("")
+        self.answer.set("n")
 
         self.question_label = Label(parent, text = "Question: Who is the prime minister of NZ?")
         self.question_label.grid(column = 0, row = 0)
@@ -15,8 +15,17 @@ class MultiChoice:
         
         z = 0
         for x in(answer_list):
-            Radiobutton(f1, text = x, value = x, variable = self.answer).grid(row = z, sticky = W)
+            Radiobutton(f1, text = x, value = x, variable = self.answer, command = self.display_answer).grid(row = z, sticky = W)
             z += 1
+
+        self.output_label = Label()
+        self.output_label.grid(pady = (0, 5))
+
+    def display_answer(self):
+        if self.answer.get() == "Jacinda Adern": 
+            self.output_label.configure(text = "Correct")
+        else:
+            self.output_label.configure(text = "Incorrect")
 
 if __name__ == "__main__":
     root = Tk()
